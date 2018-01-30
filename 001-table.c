80a26735d6a66ad2607a8596873212c485e0a302
oneface <jiangxj@xiaolj.com>
2018-01-30 18:26:34 +0800

    001-线性表实例

--- 001-table.c
+++ 001-table.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct _table_data {
 	int *data;
 	unsigned int length;
 	unsigned int size;
 }table_data;
+
+static void table_show(table_data *tdata)
+{
+	int i;
+
+	for (i = 0; i < tdata->length; i++) {
+		printf("%d->", tdata->data[i]);
+	}
+	printf("\n");
+}
 
 static void table_init(table_data **tdata, unsigned int size)
 {
@@ -23,16 +34,16 @@ static void table_init(table_data **tdata, unsigned int size)
 		exit(-1);	
 	}
 
-	memset((*tdata)->data, 0x0, size);
-	(*tdata)->length = 1;
+	memset((*tdata)->data, 0xf, size);
+	(*tdata)->length = 0;
 }
 
 static void table_insert(table_data *tdata, unsigned int loc, int data)
 {
 	int i;
 
-	if (loc < 0 || loc > tdata->length + 1) {
-		printf("locaction is invalid\n");
+	if (loc < 0 || loc >= tdata->length + 1) {
+		printf("locaction (%d) is invalid\n", loc);
 		exit(-1);
 	}
 
@@ -44,22 +55,50 @@ static void table_insert(table_data *tdata, unsigned int loc, int data)
 		}
 	}
 
-	for (i = tdata->length - 1; i > loc; i--) {
-		printf("--\n");
-		tdata->data[i+1] = tdata->data[i];
+	for (i = tdata->length; i > loc; i--) {
+		tdata->data[i] = tdata->data[i-1];
 	}
 	tdata->data[loc] = data;
 	tdata->length++;
+	table_show(tdata);
 }
 
-static void table_show(table_data *tdata)
+static void table_modify(table_data *tdata, unsigned int loc, int data)
+{
+	if (loc < 0 || loc >= tdata->length + 1) {
+		printf("locaction (%d) is invalid\n", loc);
+		exit(-1);
+	}
+
+	tdata->data[loc] = data;
+}
+
+static void table_delet(table_data *tdata, unsigned int loc)
+{
+	int i;
+
+	if (loc < 0 || loc >= tdata->length + 1 || tdata->length <= 0) {
+		printf("locaction (%d) is invalid\n", loc);
+		exit(-1);
+	}
+
+	for (i = loc; i < tdata->length-1; i++) {
+		tdata->data[i] = tdata->data[i+1];
+	}
+	tdata->length--;
+	table_show(tdata);
+}
+
+static int table_select(table_data *tdata, int data)
 {
 	int i;
 
 	for (i = 0; i < tdata->length; i++) {
-		printf("%d->", tdata->data[i]);
+		if (tdata->data[i] == data) {
+			return i;
+		}
 	}
-	printf("\n");
+	return -1;
 }
 
 int main()
@@ -71,14 +110,17 @@ int main()
 
 	table_insert(tdata, 0, 9);
 	table_insert(tdata, 0, 8);
-	table_insert(tdata, 0, 3);
+	table_insert(tdata, 1, 3);
 	table_insert(tdata, 0, 1);
+	table_insert(tdata, 4, 2);
 
-	//table_modify(tdata, 1, 7);
+	table_modify(tdata, 1, 7);
 
-	//table_delet(tdata, 3);
+	table_delet(tdata, 5);
+	table_delet(tdata, 0);
 
-	//table_grep(tdata, 5);
+	unsigned int loc = table_select(tdata, 7);
+	printf("loc is %d\n", loc);
 
 	table_show(tdata);
 
