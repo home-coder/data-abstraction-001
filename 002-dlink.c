476312f6ba41fb6edb69a193c829eca4df8d1130
oneface <jiangxj@xiaolj.com>
2018-02-05 12:43:57 +0800

    dlink: 修改和查找功能添加

--- 002-dlink.c
+++ 002-dlink.c
@@ -115,6 +115,49 @@ static void dlink_delete(dlink_data *dldata, unsigned int loc)
 	denode = NULL;
 }
 
+static void dlink_amend(dlink_data *dldata, unsigned int loc, char data)
+{
+	printf("amend loc %d data %c-->\n", loc, data);
+	int i = 0;
+	dlink_data *dlp = dldata;
+	if (!dlp) {
+		printf("dlink is not exsit\n");
+		return ;
+	}
+	
+	for (; i < loc; i++) {
+		dlp = dlp->next;
+		if (!dlp) {
+			printf("amend loc %d is invalid\n", loc);
+			return ;
+		}
+	}
+	if (!dlp->next) {
+		printf("amend loc %d is invalid\n", loc);
+		return ;
+	} else {
+		dlp->next->data = data;
+	}
+}
+
+static int dlink_select(dlink_data *dldata, char data)
+{
+	int loc = 0;
+	dlink_data *dlp = dldata;
+	if (!dlp) {
+		printf("dlink is not exsit\n");
+		return -1;
+	}
+
+	for (dlp = dlp->next; dlp != NULL; dlp = dlp->next) {
+		if (dlp->data == data) {
+			return loc;
+		}
+		loc++;
+	}
+	return -1;
+}
+
 int main()
 {
 	dlink_data *dldata;
@@ -125,7 +168,16 @@ int main()
 	dlink_insert(dldata, 0, 'e');
 	dlink_insert(dldata, 0, 'm');
 	dlink_insert(dldata, 3, 'n');
+	dlink_insert(dldata, 3, 'n');
+	dlink_insert(dldata, 3, 'n');
 	dlink_insert(dldata, 0, 'q');
+	dlink_insert(dldata, 2, 't');
+	dlink_insert(dldata, 2, 't');
+	dlink_insert(dldata, 2, 't');
+	dlink_insert(dldata, 3, 'n');
+	dlink_insert(dldata, 3, 'n');
+	dlink_insert(dldata, 3, 'n');
+	dlink_insert(dldata, 3, 'n');
 	dlink_insert(dldata, 2, 't');
 	dlink_show(dldata);
 
@@ -144,12 +196,18 @@ int main()
 	dlink_delete(dldata, 0);
 	dlink_show(dldata);
 
-#if 0
-	dlink_amend();
+	dlink_amend(dldata, 3, 'o');
+	dlink_amend(dldata, 0, 'i');
+	dlink_amend(dldata, 7, 'p');
 	dlink_show(dldata);
 
-	dlink_select();	
-	dlink_show(dldata);
-#endif
+	int loc;
+	loc = dlink_select(dldata, 'p');
+	printf("select 'p' loc in %d\n", loc);
+	loc = dlink_select(dldata, 'n');
+	printf("select 'n' loc in %d\n", loc);
+	loc = dlink_select(dldata, 'i');
+	printf("select 'i' loc in %d\n", loc);
+
 	return 0;
 }
